7ae860176ff0f6078fc47b8c2ccf4b9e68739f22
Dev <dnb.ranjikaa@gmail.com>
2021-06-22 10:05:45 +0530

    added File handling to make use of old data

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import json
 import requests
+import pprint
 
 api = '858c01a1-651c-4343-8e01-89c39143b544' 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-apiInfo = {
+Headers={
+	'Accept': 'application/json'
+}
+getRequestParam = {
 	'CMC_PRO_API_KEY': api
 }
 
 
-r = requests.get(url, params = apiInfo)
-print(r)
+getRequest = requests.get(url,params=getRequestParam,headers=Headers)
+recievedDataInJson = getRequest.json()
+prettyRecievedDataInJson = pprint.pformat(recievedDataInJson)
+
+with open('cryptodata.json','w') as out:
+	out.write(prettyRecievedDataInJson)
+
+#print(recievedDataInJson['data'])
