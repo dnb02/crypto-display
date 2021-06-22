70c2a2708613ca9b91d19bd3b8ff332d08a30a75
Dev <dnb.ranjikaa@gmail.com>
2021-06-22 12:53:45 +0530

    pinpointed useful data from json

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import requests
 
 with open('api.txt','r') as inp:
     api = inp.read(36) 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=1'
 
 Headers={
 	'Accept': 'application/json'
@@ -19,4 +20,6 @@ recievedDataInJson = getRequest.json()
 
 with open('cryptodata.json','w') as out:
 	json.dump(recievedDataInJson, out, indent= 4, sort_keys= True)
+
+print(recievedDataInJson["data"]["1"]["quote"]["USD"]["percent_change_1h"])
 
