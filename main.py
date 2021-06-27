96ba1aa1d007c9d6951d8324706d175aab80aad3
Dev <dnb.ranjikaa@gmail.com>
2021-06-27 12:56:00 +0530

    Added DropDownBox to enable users to choose the Crypto the want to track

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import tkinter
 class application:
 
 	def getValue(self):
+		
+		with open('mapping.json') as inp:
+			trackableCrypto=json.load(inp)
+		strackableCryptoNames = list(trackableCrypto.keys())
+		
+
 		with open('api.txt','r') as inp:
 			api = inp.read(36) 
 		url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
@@ -13,17 +19,31 @@ class application:
 		}
 		getRequestParam = {
 			'CMC_PRO_API_KEY': api,
-			'id':'1'
+			'id':trackableCrypto[self.clicked.get()]
+			#'id':'1'
 		}
 		getRequest = requests.get(url,params=getRequestParam,headers=Headers)
 		recievedDataInJson = getRequest.json()
-		requestedValue = recievedDataInJson["data"]["1"]["quote"]["USD"]["percent_change_1h"]
+		#requestedValue = recievedDataInJson["data"]["1"]["quote"]["USD"]["percent_change_1h"]
+		requestedValue = recievedDataInJson["data"][trackableCrypto[self.clicked.get()]]["quote"]["USD"]["percent_change_1h"]
 		self.changeDisplayValue.set(str(requestedValue))
 		
 	def gui(self):
+		with open('mapping.json') as inp:
+			trackableCrypto=json.load(inp)
+		trackableCryptoNames = list(trackableCrypto.keys())
+		
 		mainWindow = tkinter.Tk()
+		
 		self.changeDisplayValue = tkinter.StringVar(mainWindow)
 		self.changeDisplayValue.set('0')
+		
+		self.clicked= tkinter.StringVar()
+		self.clicked.set(trackableCryptoNames[0])
+		
+
+		dropDownBox = tkinter.OptionMenu(mainWindow,self.clicked,*trackableCryptoNames).pack()
+		
 		button = tkinter.Button(mainWindow, text ="Get Value", command = self.getValue ).pack()
 		display = tkinter.Label(mainWindow,textvariable = self.changeDisplayValue ).pack()
 		mainWindow.mainloop()
